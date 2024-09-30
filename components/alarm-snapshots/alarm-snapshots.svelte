a826584b000d2038185aee5df31c6019d3a5fef4
Hicham Sahla <hichamsahla91@gmail.com>
2024-09-30 11:31:48 +0200

    Ädded copy clip icon back

--- components/alarm-snapshots/alarm-snapshots.svelte
+++ components/alarm-snapshots/alarm-snapshots.svelte
@@ -571,7 +571,7 @@
             <table class="base-table">
               <thead>
                 <tr>
-                  <th class="id-column key-column">ID</th>
+                  <th class="id-column">ID</th>
                   <th class="key-column">Alarm</th>
                   <th class="key-column">Date</th>
                   <th class="key-column">Severity</th>
@@ -580,7 +580,44 @@
               <tbody>
                 {#each filteredOccurrences as occurrence}
                   <tr on:click={() => selectOccurrence(occurrence)}>
-                    <td class="id-column">{occurrence.publicId}</td>
+                    <td class="id-column">
+                      <span>{occurrence.publicId}</span>
+                      <button
+                        class="copy-button {copySuccess[occurrence.publicId]
+                          ? 'success'
+                          : ''}"
+                        on:click|stopPropagation={() =>
+                          copyToClipboard(occurrence.publicId)}
+                      >
+                        {#if copySuccess[occurrence.publicId]}
+                          <!-- Display a check icon on success -->
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 -960 960 960"
+                            width="24px"
+                            fill="#4caf50"
+                          >
+                            <path
+                              d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
+                            />
+                          </svg>
+                        {:else}
+                          <!-- Original copy icon -->
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 -960 960 960"
+                            width="16px"
+                            fill="currentColor"
+                          >
+                            <path
+                              d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"
+                            />
+                          </svg>
+                        {/if}
+                      </button>
+                    </td>
                     <td>{occurrence.name}</td>
                     <td>{formatDateForTable(occurrence.occurredOn.fullDate)}</td
                     >
@@ -619,6 +656,7 @@
       color: #555; // Hover color
       animation: pulse 1s infinite;
     }
+
     @keyframes pulse {
       0% {
         opacity: 1;
@@ -630,6 +668,7 @@
         opacity: 1;
       }
     }
+
     &.success svg {
       fill: #4caf50; // Green color indicating success
     }
